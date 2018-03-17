75e5cb9590f03d9139a359b5f0f515d94113b61b
WangJKui <453091806@qq.com>
2018-03-17 22:20:31 +0800

    master

--- src/main/java/com/wjk/java/base/master.java
+++ src/main/java/com/wjk/java/base/master.java
@@ -1,9 +1,8 @@
 package com.wjk.java.base;
 
-public class Test {
+public class master {
 
 	public static void main(String[] args) {
-		System.out.println("teseta");
 		System.out.println("master");
 	}
 
