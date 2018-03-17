3f2c71a3dac18619a0949fafbb2bbed811c01b21
WangJKui <453091806@qq.com>
2018-03-17 22:14:12 +0800

    dev分支

--- src/main/java/com/wjk/java/base/dev.java
+++ src/main/java/com/wjk/java/base/dev.java
@@ -1,9 +1,8 @@
 package com.wjk.java.base;
 
-public class Test {
+public class dev {
 
 	public static void main(String[] args) {
-		System.out.println("dev分支");
 		System.out.println("dev分支");
 	}
 
